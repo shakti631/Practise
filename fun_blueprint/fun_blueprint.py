1327929cfa94ac1ae8d7ba1a428d3b2e794cd00a
Rob <r.elliot.silver@gmail.com>
2022-12-04 15:07:32 -0800

    modifying blueprint to handle POST request and send back msg

--- fun_blueprint/fun_blueprint.py
+++ fun_blueprint/fun_blueprint.py
@@ -6,7 +6,7 @@ fun_blueprint = Blueprint('fun_blueprint', __name__, template_folder="templates"
 def sentiment_text():
     if request.method == "GET":
         # return("<h1>HELLO</h1>")
-        return render_template("sentiment_template.html")
+        return render_template("sentiment_template.html", params={"msg": None})
     else:
         msg = request.form.get("text")
-        return f"<h1>{msg}</h1>"
+        return render_template("sentiment_template.html", params={"msg": msg})
